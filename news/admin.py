1d602a361b9c6425941aa59493a2f8d94518ffd8
kimiajam <kimiajaam@gmail.com>
2026-09-05 13:31:46 +0330

    Add author and profile models

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News
-
+from .models import News, Profile
 
 admin.site.register(News)
+admin.site.register(Profile)
--- news/models.py
+++ news/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class News(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='news/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+    job = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
